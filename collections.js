f89c1fd44c802f4693aeccfb8dc8d75e59869f26
Alito24 <147553250+Alito24@users.noreply.github.com>
2025-01-28 13:35:40 +0100

    Update collections.js

--- collections.js
+++ collections.js
@@ -3,4 +3,8 @@ const collections = [
         name: "Color Spectrum",
         items: ["Red Block", "Green Block", "Blue Block", "Yellow Block", "Orange Block", "Purple Block"],
     },
+    {
+        name: "Mini Mountain",
+        items: ["Stone", "Andesite", "Granite", "Obsidian", "Bedrock"],
+    },
 ];
